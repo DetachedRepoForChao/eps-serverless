5ce62e06d9290a2d690666c4cff2bf5f3b361951
Zainab Audu <s1229578@monmouth.edu>
2019-10-14 20:34:32 -0400

    confirm item purchase

---
+++ src/app/user/confirm-item-purchase/confirm-item-purchase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-item-purchase',
+  templateUrl: './confirm-item-purchase.component.html',
+  styleUrls: ['./confirm-item-purchase.component.css']
+})
+export class ConfirmItemPurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
