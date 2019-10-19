32a5040e450f10651e80f1146b16dbbb644594b4
chao <s1272343@monmouth.edu>
2019-10-19 17:10:46 -0400

    feedcard

---
+++ src/app/entity-store/feedcard/state/feedcard.store.ts
@@ -0,0 +1,24 @@
+import { EntityState,EntityStore, StoreConfig } from '@datorama/akita';
+import {FeedcardModel } from './feedcard.model'
+import {VISIBILITY_FILTER } from '../filter/feedcard-filter.model'
+import { Injectable} from '@angular/core'
+
+export interface FeedcardState extends EntityState<FeedcardModel>{
+  ui: {
+    filter: VISIBILITY_FILTER
+  };
+}
+
+const initialState = {
+  ui: { filter: VISIBILITY_FILTER.SHOW_ALL }
+};
+
+@StoreConfig({name: 'feedcardStore'})
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedcardStore extends EntityStore<FeedcardState, FeedcardModel> {
+  constructor() {
+    super(initialState);
+  }
+}
