61bae85511663794f3701aea5465c5d025cb7cd0
Zainab Audu <s1229578@monmouth.edu>
2019-10-25 13:13:57 -0400

    confirm purchase request

--- src/app/user/confirm-item-purchase/confirm-item-purchase.component.ts
+++ src/app/user/confirm-item-purchase/confirm-item-purchase.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {CurrentUserStore} from '../../entity-store/current-user/state/current-user.store';
+import {EntityCurrentUserQuery} from '../../entity-store/current-user/state/entity-current-user.query';
+
+
+  import { from } from 'rxjs';
+
+  declare var $: any;
 
 @Component({
   selector: 'app-confirm-item-purchase',
@@ -6,10 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm-item-purchase.component.css']
 })
 export class ConfirmItemPurchaseComponent implements OnInit {
+  componentName = 'confirm-item-purchase.component';
 
-  constructor() { }
+
+  currentUser$;
+  currentUser;
+
+  constructor ( private currentUserStore: CurrentUserStore,
+                public currentUserQuery: EntityCurrentUserQuery) { }
 
   ngOnInit() {
+    const functionName = 'ngOnInit';
+    const functionFullName = `${this.componentName} ${functionName}`;
+    console.log(`Start ${functionFullName}`);
+
+
+   
+
+
+  }
+  test() {
+
   }
 
 }
