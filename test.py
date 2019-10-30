6e2b5d95a6b69a1d6f545cdc598f83752bcc87fb
Jinhu Wang <s1260809@monmouth.edu>
2019-10-29 20:35:09 -0400

    updte

--- test.py
+++ test.py
@@ -2,21 +2,24 @@ import os;
 import time; 
 import datetime;
  
-ticks = int(time.time())
-
-f1 = open('/Users/wangjinhu/Project/eps-serverless/jinhuchange.js','r+')
-infos = f1.readlines()
-f1.seek(0,0)
-f1.write("Change Time:"+time.strftime("%Y-%m-%d %H:%M", time.localtime(ticks)))
-f1.close()
 
 
+for i in range(3):
+    ticks = int(time.time())
 
-os.system("git add .")
+    f1 = open('/Users/wangjinhu/Project/eps-serverless/jinhuchange.js','r+')
+    infos = f1.readlines()
+    f1.seek(0,0)
+    f1.write("Change Time:"+time.strftime("%Y-%m-%d %H:%M", time.localtime(ticks)))
+    f1.close()
 
 
-os.system("git commit -m 'updte' ")
+
+    os.system("git add .")
 
 
-os.system("git push origin master")
+    os.system("git commit -m 'updte' ")
+
+
+    os.system("git push origin master")
 
